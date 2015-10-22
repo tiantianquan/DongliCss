41422a7e981af6ffd5b5a542bd22b37e3ff5c00b
tiantianquan <fenqiufen@gmail.com>
2015-10-22 13:43:35 +0800

    修改 微信 BUG

--- script.js
+++ script.js
@@ -14,13 +14,16 @@ var wxData = {
   type: 'link',
 }
 
+// var getUrl = 'api/Wx'
+var getUrl = 'http://www.linzhifang.com/api/Wx'
+
 //配置微信分享接口
-$.getJSON('api/Wx',(data) =>{
-  console.log(
-    `token:${data.testToken}`,`ticket:${data.testTicket};`
-  )
+$.getJSON(getUrl,(data) =>{
+  // console.log(
+  //   `token:${data.testToken}`,`ticket:${data.testTicket};`
+  // )
   wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: data.appId, // 必填，公众号的唯一标识
     timestamp: data.timestamp, // 必填，生成签名的时间戳
     nonceStr: data.nonceStr, // 必填，生成签名的随机串
